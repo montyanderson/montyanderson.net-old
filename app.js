428692fde9b8cf78639ffa4d965e6ac163a61be0
Monty Anderson <monty.linux@gmail.com>
2015-04-28 20:00:50 +0100

    getting started

---
+++ app.js
@@ -0,0 +1,23 @@
+var express = require("express"),
+	app = express();
+
+app.use(express.static("static"));
+
+var ect = require("ect")({
+	"watch": true,
+	"root": __dirname + "/views",
+	"ext": ".ect"
+});
+
+app.set("view engine", "ect");
+app.engine("ect", ect.render);
+
+app.get("/", function(req, res) {
+	res.render("index");
+});
+
+
+var ip = process.env.OPENSHIFT_INTERNAL_IP || process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
+var port = process.env.OPENSHIFT_INTERNAL_IP || process.env.OPENSHIFT_NODEJS_PORT || process.argv[2] || 3000;
+
+app.listen(port, ip, function() {});
