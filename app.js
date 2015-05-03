7023684c5361784309ef4db3467d4c9369b01639
Monty Anderson <monty.linux@gmail.com>
2015-05-03 17:49:41 +0100

    a multitude of changes!

--- app.js
+++ app.js
@@ -18,7 +18,11 @@ app.get("/git", function(req, res) {
 	res.render("git");
 });
 
+app.get("/records", function(req, res) {
+	res.render("records");
+});
+
 var ip = process.env.OPENSHIFT_INTERNAL_IP || process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
-var port = process.env.OPENSHIFT_INTERNAL_IP || process.env.OPENSHIFT_NODEJS_PORT || process.argv[2] || 3000;
+var port = process.env.OPENSHIFT_INTERNAL_IP || process.env.OPENSHIFT_NODEJS_PORT || process.argv[2] || 8080;
 
 app.listen(port, ip, function() {});
