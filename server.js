50d3b4a4b1281487db120488975956bdcc961450
Monty Anderson <monty.linux@gmail.com>
2015-07-05 11:01:02 +0100

    added openshift internal ip

--- server.js
+++ server.js
@@ -101,8 +101,8 @@ function updateRepos() {
 updateRepos();
 setInterval(updateRepos, 5 * 60 * 1000);
 
-var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || process.argv[3] || 8080;
-var ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || process.arguments[4] || "*";
+var port = process.env.OPENSHIFT_INTERNAL_IP || process.env.OPENSHIFT_NODEJS_IP || process.argv[3] || 8080;
+var ip =  process.env.OPENSHIFT_INTERNAL_IP || process.env.OPENSHIFT_NODEJS_PORT || process.arguments[4] || "*";
 app.listen(port);
 
 console.log("Server started at port " + port + ".");
