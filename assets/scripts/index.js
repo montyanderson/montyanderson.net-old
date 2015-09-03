7a4685afdcb7a3ce016ea8b3d75f136baba2cec0
Monty Anderson <monty.linux@gmail.com>
2015-09-03 20:25:31 +0100

    tyrell now works properly on android

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -3,3 +3,18 @@ window.jQuery = window.$ = require("jquery");
 $(document).ready(function() {
     $(".parallax").parallax();
 });
+
+if(navigator.userAgent.toLowerCase().indexOf("android") > -1) {
+    $("video").each(function() {
+        if($(this).data("poster"))
+            $(this).attr("poster", $(this).data("poster"));
+    });
+}
+
+jQuery(document).on("click", "video", function(){
+        if (this.paused) {
+            this.play();
+        } else {
+            this.pause();
+        }
+});
